e36a943a9c9ead457b336e32f4b7947e784032e4
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-10-11 00:58:56 +0300

    1

---
+++ Day-2/Checkpoint-1.swift
@@ -0,0 +1,6 @@
+import SwiftUI
+
+var celcius = 15
+var fahrenheit = celcius * 9 / 5 + 32
+
+print(celcius , "Celcius is equal to" , fahrenheit , "Fahrenheit") 
--- Day-2/Sets.swift
+++ Day-2/Sets.swift
@@ -6,3 +6,5 @@ let colors2 = Set(["furkan", "ömer", "aslı", "furkan"])
 // Bu kod çalışmayacaktır çünkü set ile Array arasında bazı farklar bulunmaktadır. 
 // Setlerde aynı elemanı birden fazla yazamayız.
 // Setlerde sıralama yoktur.
+print(colors)
+print(colors2)
