8f169654064cf43f04686b01484ea6e3d5be7b11
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-10-10 23:44:10 +0300

    day - 2 some

---
+++ Day-2/Sets.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+let colors = Set(["furkan", "ömer", "aslı"])
+
+let colors2 = Set(["furkan", "ömer", "aslı", "furkan"])
+// Bu kod çalışmayacaktır çünkü set ile Array arasında bazı farklar bulunmaktadır. 
+// Setlerde aynı elemanı birden fazla yazamayız.
+// Setlerde sıralama yoktur.
---
+++ Day-2/Tuples.swift
@@ -0,0 +1,11 @@
+// Tuples birden fazla veriyi içerisinde barındırmaya yarayan bir yapıdır. 
+
+import Foundation
+
+
+let name = (first: "Furkan", last: "Anter")
+
+// name ' e ulaşmak istiyorsan name.0 veya name.1 yazman yeterli olacaktır.
+// naem.first ile de ulaşabilirsin.
+
+// Tuple içeriisndeki bir veriyi daha sonra değiştirebilirsin.
