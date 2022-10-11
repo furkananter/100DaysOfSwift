ffdd082b58f15824e6771d6a1b86b587ab5bc100
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-10-12 01:00:42 +0300

    2

--- Day-2/Day2.swift
+++ Day-2/Day2.swift
@@ -1,10 +1,32 @@
-import Cocoa
-    // hasSuffix()
+    import  Cocoa
+    // hasSuffix() - son ek demek. Son ek'i .jpg ile bitiyor mu? dedik.
+    
+    
+    let filename = "paris.jpg"
+    // print(filename.hasSuffix(".jpg"))
+    
+    let number = 120
+    // print(number.isMultiple(of: 3))
+    
+    
+    // yukarıdaki gibi number. demek yerine 120.isMultiple(of:3) diyebiliriz.
+    
+    // ! 
+    
+    var isAuth = false
+    
+    isAuth = !isAuth
+    // print(isAuth)
+    isAuth = !isAuth
+    // print(isAuth)
+    
+    
+        // toggle()
+    
+    var gameOver = false
+    // print(gameOver)
+    gameOver.toggle()
+    // print(gameOver)
 
-
-let filename = "paris.jpg"
-print(filename.hasSuffix(".jpg"))
-
-let number = 120
-print(number.isMultiple(of: 3))
-
+    
+    //  
