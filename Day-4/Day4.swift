1fc08afd0be14082cbcf1c0f7a906c31d6dcd466
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-10-14 08:42:39 +0300

    Update Day4.swift

---
+++ Day-4/Day4.swift
@@ -0,0 +1,25 @@
+import Cocoa
+
+let username = "Furkan"
+var score = 0
+
+// bunu type vererek de şöyle yapardık:
+
+let usernameTypeli: String = "Furkans"
+var scoreTypeli: Int = 0
+
+// Eğer score değişkeni gibi bir değişkenin varsa ve bu yarım puanlar da alması gerekiyorsa şöyle yapıyorsun:
+
+var scoreTyps: Double = 32.5
+
+// String, Text tutar.
+// Double, Decimal tutar.
+// Int, tüm sayıları tutar.
+// bool, True ya da False tutar.
+// Arrays (diziler) birden çok değer tutar.
+
+let playerName: String = "Asmodeus"
+
+
+
+
