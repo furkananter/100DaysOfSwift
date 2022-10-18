e57ea9dfc65cec5ea968adcf0447d1398bfef0de
furky.eth <anterfurkan@gmail.com>
2022-10-18 18:04:39 +0300

    Update Day7.swift

--- Day-7/Day7.swift
+++ Day-7/Day7.swift
@@ -42,14 +42,46 @@ import Cocoa
 
 // }
 
+// okulla alakalı konuyla alakası yok. okulda öylesine yazdığım bir kod
+// func RassalSayi(number: Int) -> Array<Int> {
+//     var array = [Int]()
+//     for _ in 1...number{
+//         array.append(Int.random(in: 1000...6000))
+//     }
+//     return array
+// }
 
-func RassalSayi(number: Int) -> Array<Int> {
-    var array = [Int]()
-    for _ in 1...number{
-        array.append(Int.random(in: 1000...6000))
-    }
-    return array
-}
+// let result = RassalSayi(number: 10)
+// print(result)
 
-let result = RassalSayi(number: 10)
-print(result)
+// Pisagor
+// func pythogorias(number1: Double, number2: Double) -> Double  {
+//     let input = number1 * number1 + number2 * number2
+//     let root = sqrt(input)
+//     return root
+// }
+// let c = pythogorias(number1: 5, number2: 10)
+// print(c)
+
+// Pisagor Kısa:
+// func pythogorias(number1: Double, number2: Double) -> Double  {
+//    sqrt(number1 * number1 + number2 * number2)
+// }
+// let c = pythogorias(number1: 7, number2: 10)
+// print(c)
+
+// MULTİPLE VALUES FROM FUNCTİONS
+
+// func isUpperCase(string: String) -> Bool {
+//     string == string.uppercased()
+// }
+
+// let c = isUpperCase(string: "HELLO")
+// print(c)
+
+// Taklmaca
+// func getUser() -> [String: String] {
+//     ["name": "John", "surname": "Doe"]
+// }
+// let user = getUser()
+// print("Name :\(user["name", default:"Anonymous"]) Surname: \(user["surname", default: "Anonymous"])")
