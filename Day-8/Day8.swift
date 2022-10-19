71ed864e9a6f91901dac50e517240bf5a6f9eb87
furky.eth <anterfurkan@gmail.com>
2022-10-20 01:07:58 +0300

    Update Day8.swift

--- Day-8/Day8.swift
+++ Day-8/Day8.swift
@@ -6,3 +6,35 @@ func printTimesTable(for number: Int, end: Int = 12) {
 }
 printTimesTable(for: 4, end: 20)
 
+
+
+// Ekstra
+
+var Characters = ["1","2","3","4"]
+print(Characters.count)
+Characters.removeAll(keepingCapacity: true)
+print(Characters.count)
+
+// ERRORS: 
+
+enum PasswordErrors: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password.count < 5 {
+        throw PasswordErrors.short
+    }
+    if password == "12345" {
+        throw PasswordErrors.obvious
+    }
+    if  password.count < 8{
+        print("Ok")
+    }else if password.count < 10{
+        print("Good")
+    }else {
+        return "Excellent"
+    }
+
+}
+
