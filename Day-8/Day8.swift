e5625e3a923c84f92287bd4ea6351d801a792557
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-10-20 08:20:26 +0300

    Update Day8.swift

--- Day-8/Day8.swift
+++ Day-8/Day8.swift
@@ -1,10 +1,10 @@
 import Cocoa
-func printTimesTable(for number: Int, end: Int = 12) {
-    for i in 1...end{
-        print("\(i) x \(number) is \(i * number)")
-    }
-}
-printTimesTable(for: 4, end: 20)
+// func printTimesTable(for number: Int, end: Int = 12) {
+//     for i in 1...end{
+//         print("\(i) x \(number) is \(i * number)")
+//     }
+// }
+// printTimesTable(for: 4, end: 20)
 
 
 
