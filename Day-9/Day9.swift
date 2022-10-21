c6300f92086e3b125a3f4f169c1696d1034a23c8
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-10-21 18:35:02 +0300

    Update Day9.swift

---
+++ Day-9/Day9.swift
@@ -0,0 +1,41 @@
+import Cocoa
+
+// func getUser() {
+//     print("user")
+// }
+// getUser()
+
+// var userss = getUser
+// userss()
+
+// degiskeni fonksiyona dönüstürmek
+
+let style = {
+    print("güzel özellikmiş.")
+}
+
+style()
+
+// in kullanmak zorundayız çünkü diğer türlü bunu parametreli bir fonksiyona döndüremeyiz.
+
+
+// let user = { (name: String) -> String in
+//     "Hello \(name)"
+// }
+
+
+// *
+var greetCopy: () -> Void = style
+
+
+func getVal(for id: Int) -> String {
+    if id == 1964 {
+       return "Furkans"
+    }else{
+       return "Şevval"
+    }
+}
+
+let data: (Int) -> String = getVal
+let user = data(19624)
+print(user)
