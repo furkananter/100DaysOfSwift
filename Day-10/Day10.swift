4f34ff7dea07e76a9272a404c70dc93e2b7ddc56
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-10-24 19:18:20 +0300

    az kaldı

--- Day-10/Day10.swift
+++ Day-10/Day10.swift
@@ -14,3 +14,32 @@ struct Album {
 let red: Album = Album(title: "Red", artist: "Furkans", year: 2020)
 
 red.printSummary()
+
+struct Employee {
+    let name: String
+    var VacationRemaining: Int
+
+    mutating func TakeVacation(days: Int){
+        if VacationRemaining > days {
+            VacationRemaining -= days
+            print("Enjoy your vacation!")
+        }else{
+            print("Oppss! You don't have enough vacation days.")
+        } 
+    }
+}
+
+var furkan = Employee(name: "Furkan", VacationRemaining: 10)
+
+
+var archer = Employee(name: "Archer", VacationRemaining: 12)
+archer.TakeVacation(days: 5)
+print(archer.VacationRemaining)
+
+/*
+    * Variables and constants that belong to structs are called properties.
+    * Functions that belong to structs are called methods.
+*/
+
+var archer1: Employee = Employee(name: "ARcher", VacationRemaining: 14)
+var archer2: Employee = Employee.init(name: "Archer2", VacationRemaining: 112)
