466ad2bde3ea5cda8782de30290ace8e42e82d6c
furky.eth <anterfurkan@gmail.com>
2022-10-27 01:11:04 +0300

    Update Day11.swift

--- Day-11/Day11.swift
+++ Day-11/Day11.swift
@@ -35,3 +35,37 @@ import Foundation
 
 // STATIC PROPERTIES AND METHODS
 
+
+struct School {
+    static var studentCount = 0
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+
+var ogrenci = School.add(student: "Taylor Swift")
+print(ogrenci.studentCount)
+
+
+
+//
+
+
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "settings.json"
+    static let homeUrl = "https://www.hackingwithswift.com"
+}
+
+
+//
+
+
+struct Employee {
+    let username: String
+    let password: String
+
+    static let example = Employee(username: "asmodeus", password: "bilmiyorumkine")
+}
+
