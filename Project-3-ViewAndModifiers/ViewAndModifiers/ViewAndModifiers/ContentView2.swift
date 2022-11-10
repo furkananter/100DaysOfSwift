9986212d747cd0ffcbb6458c2942a082d95a5ba0
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-11-11 00:37:41 +0300

    finished

---
+++ Project-3-ViewAndModifiers/ViewAndModifiers/ViewAndModifiers/ContentView2.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView2.swift
+//  ViewAndModifiers
+//
+//  Created by Furkan Anter on 11/11/22.
+//
+
+import SwiftUI
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int,Int) -> Content
+    
+    var body: some View {
+        // code goes here
+        VStack{
+            ForEach(0..<rows, id: \.self){row in
+                HStack{
+                    ForEach(0..<columns, id: \.self){column in
+                        content(row,column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView2: View {
+    var body: some View {
+        // Do Something
+        
+        GridStack(rows: 4, columns: 4){ row, col in
+            HStack{
+                Image(systemName: "\(row * 4 + col).circle")
+                Text("R \(row), C \(col)")
+                    
+            }
+            .frame(width:70,height: 100)
+            
+        }
+    }
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView2()
+    }
+}
