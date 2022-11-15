7b8ebb41a2dc8ec00cfc7ab010454fa9c5e67e95
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-11-15 17:39:37 +0300

    Very İmprovement

--- Project-4-RockPaperScissors/RockPaperScissor/RockPaperScissor/ContentView.swift
+++ Project-4-RockPaperScissors/RockPaperScissor/RockPaperScissor/ContentView.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rock = 1
+    @State private var paper = 2
+    @State private var scissors = 3
+    @State private var images = ["Rock", "Paper", "Scissors"]
+    
     var body: some View {
-        ZStack {
-            VStack {
+        ZStack{
+            LinearGradient(colors: [Color.green, Color.yellow, Color.red] , startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
                 Text("Rock & Paper & Scissors")
+                    .font(.title)
+                    .padding(.top)
+                HStack {
+                    ForEach(0..<3){number in
+                        Button {
+                            startGame()
+                        } label: {
+                            Image(images[number])
+                                .frame(width: 100, height:50, alignment: .center)
+                                .background(.blue)
+                                .cornerRadius(20)
+                                .shadow(radius: 5)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        }
+                    }
+                }
                 
+                .padding(20)
             }
+            .background(.thinMaterial)
+            .padding(10)
         }
-        
-        .padding()
     }
+}
+func startGame(){
+    print("Game has began")
 }
 
 struct ContentView_Previews: PreviewProvider {
